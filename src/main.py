dc5176afd3d27ffd80eac8fbf3665332679d0703
almaz <almazgimaev@gmail.com>
2025-08-07 19:03:45 +0200

    Add VSCode configuration files and update environment variables

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ import supervisely as sly
 
 load_dotenv("local.env")
 
-graph = sly.solution.SolutionGraphBuilder.from_yaml("src/config/config.yaml")
+graph = sly.solution.GraphBuilder.from_yaml("src/config.yaml")
 
 app = sly.Application(layout=sly.app.widgets.Container([graph, *graph.modals]))
