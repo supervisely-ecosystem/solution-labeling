9a7ff3cce0d4164d5dd1cb15a65f825151f48f69
almaz <almazgimaev@gmail.com>
2025-08-11 21:07:08 +0200

    Add data and state restoration in main.py

--- src/main.py
+++ src/main.py
@@ -7,3 +7,6 @@ graph = sly.solution.GraphBuilder.from_yaml("src/config.yaml")
 app = sly.Application(layout=sly.app.widgets.Container([graph, *graph.modals]))
 app.call_before_shutdown(sly.solution.TasksScheduler().shutdown)
 app.call_before_shutdown(sly.solution.PubSubAsync().shutdown)
+
+# # * Restore data and state if available
+sly.app.restore_data_state(sly.env.task_id())
