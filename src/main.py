1a7cc3cf165f8fc430e7591a94b3e68e2f3f5d1b
almaz <almazgimaev@gmail.com>
2025-08-23 23:17:26 +0200

    update config; update graph loading method in main.py; add static directory to .gitignore

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from src.state import State
 state = State()  # todo: temp
 graph = sly.solution.GraphBuilder()
 if sly.fs.file_exists("src/config.yaml"):
-    graph.from_yaml("src/config.yaml")
-    graph.reload()
+    graph.load_yaml("src/config.yaml")
 
 app = sly.Application(layout=sly.app.widgets.Container([graph, *graph.modals]), static_dir="static")
 app.call_before_shutdown(sly.solution.TasksScheduler().shutdown)
