d3af247c5a2454e36aad7bfd89a57cdc631ff59c
almaz <almazgimaev@gmail.com>
2025-09-01 11:34:21 +0200

    fix static path

--- src/main.py
+++ src/main.py
@@ -20,4 +20,4 @@ app.call_before_shutdown(sly.solution.PubSubAsync().shutdown)
 # # * Restore data and state if available
 sly.app.restore_data_state(sly.env.task_id())
 
-graph._prepare_ui_static(static_dir)
+graph._prepare_ui_static(app.get_static_dir())
