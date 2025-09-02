ec907a6b223bd0e97ddb76a98f2553788d448965
cxnt <pavelbartsits@gmail.com>
2025-09-02 17:42:50 +0400

    update training evaluation report name in config.yaml; refactor main.py to use sly_solution for graph and task management

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 import supervisely as sly
+import supervisely.solution as sly_solution
 from src.state import State
 
 state = State()  # todo: temp
-graph = sly.solution.GraphBuilder()
+graph = sly_solution.GraphBuilder()
 if sly.fs.file_exists("src/config.yaml"):
     graph.load_yaml("src/config.yaml")
 
 static_dir = "static"
-sly.fs.mkdir(static_dir, True)
+if not sly.fs.dir_exists(static_dir):
+    sly.fs.mkdir(static_dir, True)
 
 app = sly.Application(
     layout=sly.app.widgets.Container([graph, *graph.modals], gap=0),
     static_dir="static",
     show_header=False,
 )
-app.call_before_shutdown(sly.solution.TasksScheduler().shutdown)
-app.call_before_shutdown(sly.solution.PubSubAsync().shutdown)
+app.call_before_shutdown(sly_solution.TasksScheduler().shutdown)
+app.call_before_shutdown(sly_solution.PubSubAsync().shutdown)
 
 # # * Restore data and state if available
 sly.app.restore_data_state(sly.env.task_id())
